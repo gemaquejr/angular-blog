08284fbcea2c043c4fc21e856220390a9882e431
gemaquejr <gemaquejr@hotmail.com>
2023-09-06 00:22:37 -0300

    Adiciona base do componente menu-title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuBarComponent
+    MenuBarComponent,
+    MenuTitleComponent
   ],
   imports: [
     BrowserModule,
