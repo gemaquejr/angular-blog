d7175a9ce8e591b4062d3a6595b48012d735d556
gemaquejr <gemaquejr@hotmail.com>
2023-09-06 00:24:12 -0300

    Adiciona base do componente small-card

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuBarComponent,
     MenuTitleComponent,
-    BigCardComponent
+    BigCardComponent,
+    SmallCardComponent
   ],
   imports: [
     BrowserModule,
