0d82a5908b0d7d458045f56b87193cd6c0f1c409
gemaquejr <gemaquejr@hotmail.com>
2023-09-07 17:11:13 -0300

    Adiciona logica dinâmica em content-component

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
+  photoCover: string = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIXpWe6SY5ZOS-mqaaDYKXalotc2dHwL7W2Q&usqp=CAU"
+  contentTitle: string = "NOVA FASE DA MARVEL SERÁ MELHOR QUE A PRIMEIRA ?"
+  contentDescription: string = "Marvel anuncia o inicio da fase 2 do UCM"
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 
 }
